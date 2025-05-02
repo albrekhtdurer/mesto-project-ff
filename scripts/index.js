9b7b5c29e1e0187c6ef33f3449d5d3a82075580c
Ekaterina Novikova <fearnocait@gmail.com>
2025-05-02 22:57:05 +0300

    [feat] add logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function renderCard(cardData, deleteFunc) {
+  if (typeof cardData !== 'object' || !cardData.name || !cardData.link) {
+    return;
+  }
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__title').textContent = cardData.name;
+  card.querySelector('.card__image').src = cardData.link;
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteFunc);
+  return card;
+}
 
-// @todo: Функция удаления карточки
+function deleteCard(evt) {
+  const cardToDelete = evt.currentTarget.closest('.card');
+  cardToDelete.remove();
+}
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function(card) {
+  const renderedCard = renderCard(card, deleteCard);
+  if (renderedCard) {
+    cardsList.append(renderedCard);
+  }
+});
