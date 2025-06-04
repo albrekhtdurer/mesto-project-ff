9214515c6a102bbe606911dced9bd707708053a7
Ekaterina Novikova <fearnocait@gmail.com>
2025-06-04 21:31:13 +0300

    [feat] добавили добавление карточки и проставление лайка

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {renderCard, deleteCard, addCardsOnPage} from './scripts/card'
+import {renderCard, deleteCard, addCardOnPage} from './scripts/card'
 import {initialCards} from './scripts/cards'
 import { closeModal, openModal } from './scripts/modal'
 
@@ -16,7 +16,9 @@ import likeActive from './images/like-active.svg'
 import likeInactive from './images/like-inactive.svg'
 import logo from './images/logo.svg'
 
-addCardsOnPage(initialCards);
+initialCards.forEach(function(card) {
+  addCardOnPage(card, 'end');
+})
 
 const popupEdit = document.querySelector('.popup_type_edit');
 const popupAdd = document.querySelector('.popup_type_new-card');
@@ -26,21 +28,32 @@ const closeButtons = document.querySelectorAll('.popup__close');
 //TODO: переименовать тут по шаблону (?) и подумать про модуль
 const formEdit = document.forms['edit-profile'];
 const formEditName = formEdit.elements['name'];
-const formEditDescription = formEdit.elements.description;
+const formEditDescription = formEdit.elements['description'];
 const currentName = document.querySelector('.profile__title').textContent;
 const currentDescription = document.querySelector('.profile__description').textContent;
 formEditName.value = currentName;
 formEditDescription.value = currentDescription;
+const formAdd = document.forms['new-place'];
+const inputPlaceName = formAdd.elements['place-name'];
+const inputLink = formAdd.elements['link'];
 
-function handleFormSubmit(evt) {
+
+function handleFormEditSubmit(evt) {
   evt.preventDefault();
   document.querySelector('.profile__title').textContent = formEditName.value;
   document.querySelector('.profile__description').textContent = formEditDescription.value;
   closeModal(popupEdit);
 }
 
-formEdit.addEventListener('submit', handleFormSubmit); 
+formEdit.addEventListener('submit', handleFormEditSubmit);
 
+formAdd.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  const cardData = {name: inputPlaceName.value, link: inputLink.value};
+  addCardOnPage(cardData, 'start');
+  formAdd.reset();
+  closeModal(popupAdd);
+});
 
 buttonEdit.addEventListener('click', function() {
   openModal(popupEdit);
@@ -50,7 +63,7 @@ buttonAdd.addEventListener('click', function() {
   openModal(popupAdd);
 });
 
-closeButtons.forEach(button => {
+closeButtons.forEach(function(button) {
   button.addEventListener('click', function(evt) {
     const popupToClose = evt.target.closest('.popup_is-opened');
     closeModal(popupToClose);
--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,14 +1,15 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
-const cardsList = document.querySelector('.places__list');
+const displayedCards = document.querySelector('.places__list');
 
 /**
  * @function renderCard - создает готовый к выводу элемент карточки
  * @param   {{name: string, link: string}} cardData   данные карточки
  * @param   {function}                     deleteFunc функция удаления карточки
+ * @param   {function}                     likeFunc   функция проставления лайка
  * @returns {HTMLElement}                             элемент карточки
  */
-export function renderCard(cardData, deleteFunc) {
+export function renderCard(cardData, deleteFunc, likeFunc) {
   if (typeof cardData !== 'object' || !cardData.name || !cardData.link) {
     return;
   }
@@ -18,6 +19,8 @@ export function renderCard(cardData, deleteFunc) {
   card.querySelector('.card__image').alt = cardData.name;
   const deleteButton = card.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', deleteFunc);
+  const likeButton = card.querySelector('.card__like-button');
+  likeButton.addEventListener('click', likeFunc);
   return card;
 }
 
@@ -30,12 +33,17 @@ export function deleteCard(evt) {
   cardToDelete.remove();
 }
 
+export function addLike(evt) {
+  evt.target.classList.toggle('card__like-button_is-active');
+}
 
-export function addCardsOnPage(cards) {
-  cards.forEach(function (card) {
-  const renderedCard = renderCard(card, deleteCard);
-  if (renderedCard) {
-    cardsList.append(renderedCard);
+export function addCardOnPage(card, position) {
+  console.log('tralalero tralala');
+  const renderedCard = renderCard(card, deleteCard, addLike);
+  if (position === 'start') {
+    displayedCards.prepend(renderedCard);
+  } else if (position === 'end') {
+    displayedCards.append(renderedCard);
   }
-})
-};
+}
+
